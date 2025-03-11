1d4eeb3b31f947d1d07db172eb1d16b2fba6ac04
Agnès Cappello <senga.ds@gmail.com>
2025-03-11 15:31:21 +0100

    crud with mongodb

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,4 +1,4 @@
-import { removeListener } from "process";
+//import { removeListener } from "process";
 
 require('dotenv').config();
 const {MongoClient} = require('mongodb');
@@ -13,17 +13,54 @@ async function connectDB() {
     console.log('Connected to MongoDB');
     const db = client.db('gamerz-db');
     const collection = db.collection('users');
-    const insertUser = await collection.insertOne({
-        username: 'Gerardo', 
-        password: 'securepassword', 
-        email: 'gerard@gmail.com',
-        avatar: 'https://images.icon-icons.com/1378/PNG/512/avatardefault_92824.png', 
-        status: 'pending', 
-        role : 'admin',
-        birthdate: new Date('1990-01-01')
-    });
-    console.log('User inserted:', insertUser);
-    // const user = {name: 'John', age: 25};
+    //create = ok
+    // try{
+    //     const insertUser = await collection.insertOne({
+    //     username: 'Roberto', 
+    //     password: 'securepassword', 
+    //     email: 'roberto@gmail.com',
+    //     avatar: 'https://images.icon-icons.com/1378/PNG/512/avatardefault_92824.png', 
+    //     status: 'pending', 
+    //     role : 'admin',
+    //     birthdate: new Date('1990-01-01')
+    //     });
+    //     console.log('Inserted user: ', insertUser);
+
+    // } catch (e) {
+    //     throw e;
+    // }
+    //read
+    try{
+        // const findUser = await collection.findOne({username:'Gerardo'});
+        // console.log('Found user: ', findUser);
+       // const findAdmins = await collection.find({role:'admin'});
+      //  console.log(await findAdmins.toArray());
+    } catch (e) {
+        throw e;
+    }
+    //update
+    try{
+        // const updateGerardo = await collection.updateOne({username:'Fransisco'}, {$set: {role: 'user'}});
+        // console.log(updateGerardo);
+        const updateRoles = await collection.updateMany({role:'user'}, {$set: {role: 'admin'}});
+        console.log(updateRoles);
+
+    } catch (e) {
+        throw e;
+    }
+    //delete
+    try{
+         const deleteUser = await collection.deleteOne({username:'Fransisco'});
+        console.log(deleteUser);
+       // const deleteAllUsers = await collection.deleteMany({});
+       // console.log(deleteAllUsers);
+       //deleteEvryOne pour tout !
+       
+
+    } catch (e) {
+        throw e;
+    }
+
     return ('done');
 }
 
@@ -31,3 +68,6 @@ connectDB()
 .then(console.log)
 .catch(console.error)
 .finally(() => client.close());
+
+
+
