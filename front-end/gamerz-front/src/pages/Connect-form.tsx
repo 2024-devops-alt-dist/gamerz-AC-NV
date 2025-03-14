8c64d3d05164e43a99f53530e0a8faec711cdbf7
Agnès Cappello <senga.ds@gmail.com>
2025-03-14 18:02:17 +0100

    feat - auth (back<=>front)

--- front-end/gamerz-front/src/pages/Connect-form.tsx
+++ front-end/gamerz-front/src/pages/Connect-form.tsx
@@ -1,24 +1,54 @@
-import Hero from "../components/Hero.tsx";
+//import Hero from "../components/Hero.tsx";
 
 
 function Connexion() {
+	console.log("hello from Connexion");
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		const formData = new FormData(e.target as HTMLFormElement);
+        const username = formData.get("username");
+        const password = formData.get("password");
+		console.log("Données envoyées :", { username, password });
+
+
+		const response = await fetch('http://localhost:5006/auth', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			credentials: 'include',
+			body: JSON.stringify({ username, password }),
+		});
+		
+        if (!response.ok) {
+            throw new Error("Connexion échouée");
+        }
+		console.log(response);
+	}
+
+
+	
 	return (
 		<>
 			<div className="w-full h-screen flex items-center justify-center">
-				<form className="w-full md:w-1/3 rounded-lg bg-white/10">
+				<form className="w-full md:w-1/3 rounded-lg bg-white/10" onSubmit={handleSubmit}>
 					<h1 className="text-4xl text-center text-gray-200 mt-10 mb-10">Connexion</h1>
 					<div className="px-12 pb-10">
 						<div className="w-full mb-2">
 							<div className="flex items-center">
 								<input
 									type="text"
-									placeholder="Adresse email"
+									name="username"
+									placeholder="username"
 									className="w-full border rounded px-3 py-2 text-gray-700 focus:outline-none bg-white"
 								/>
 							</div>
 							<div className="flex items-center mt-10">
 								<input
 									type="password"
+									name="password"
 									placeholder="Mot de passe"
 									className="w-full border rounded px-3 py-2 text-gray-700 focus:outline-none bg-white"
 								/>
