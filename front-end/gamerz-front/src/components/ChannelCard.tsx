2272dfa3ff30bbe0cd1f108f2597c9912acd29f5
Praksix <nathan.vianey@gmail.com>
2025-04-07 14:20:12 +0200

    ajout page channels list

---
+++ front-end/gamerz-front/src/components/ChannelCard.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+function ChannelCard({}: {}) {
+    return (
+        <>
+        <li className='relative flex w-full gap-4  bg-[#3F6965] rounded-[20px] hover:transition'>
+                       
+                       <Link to={'/'} className='w-30 overflow-hidden rounded-left-[20px]'>
+                            <img src="public/thumbnail.png" alt="Valorant" className='object-cover rounded-l-[20px] my-auto w-30 h-30' />
+                        </Link>
+                        <div className='flex flex-col px-2 py-3'>
+                            <div className='w-full'>
+                                <Link to={'/'} className='font-semibold md:text-4xl text-[#F1E1A7]'>
+                                    <h3>VALORANT</h3>
+                                </Link>
+                                <p className='text-sm text-white '>
+                                  216 joueurs
+                                </p>
+                            </div>
+                            <div className='flex items-left justify-left mt-1 gap-1'>
+                                <div className='mt-1 text-sm text-gray-900 bg-[#1EDCB3] rounded-full px-6 py-1'>
+                                    Rejoindre
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </li>
+
+
+     </>
+   
+    );
+}
+
+export default ChannelCard;
